54e8f26d276df5c0b4a3d2e4c8eb4ce8e2133116
Bheemanagowda <bheemanagowda07@mail.com>
2025-07-04 15:49:19 +0530

    vite conig file in changes

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Vite config for production (Vercel-ready)
 export default defineConfig({
-  
   plugins: [react()],
-    server: {
-    port: 5174, // or whatever port you're using
-    host: true, // VERY IMPORTANT for external access
-    allowedHosts: ["edb0-2406-7400-56-2304-dd29-eed4-ea54-429.ngrok-free.app", '.loca.lt'], // allow all loca.lt URLs
-    // allowedHosts: ['.loca.lt'], // allow all loca.lt URLs
-  },
-   build: {
-    chunkSizeWarningLimit: 1000 // <- Increase limit from default 500 KB
-  }
- 
-});
+})
